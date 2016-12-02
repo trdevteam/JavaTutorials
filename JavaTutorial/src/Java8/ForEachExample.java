0caa733f72dcd97b8a2b72bb9ad8dd91896faf52
trdevteam <tarikmurat.islam@gmail.com>
2016-12-02 08:28:18 +0200

    Java8ForEachExample

--- JavaTutorial/src/Java8/ForEachExample.java
+++ JavaTutorial/src/Java8/ForEachExample.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.lang.Integer;
 
-public class Java8ForEachExample {
+public class ForEachExample {
 
 	public static void main(String[] args) {
 		
