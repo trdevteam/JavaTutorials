a8e2fd1e8d4f3a0bf19c763e7f7b79d6be331bf5
trdevteam <tarikmurat.islam@gmail.com>
2016-12-02 08:26:07 +0200

    ChainOfResponsibility+Java8 (Foreach)

---
+++ JavaTutorial/src/DesignPatterns/Behavioral/ChainOfResponsibility/TutorialsPoint/FileLogger.java
@@ -0,0 +1,14 @@
+package DesignPatterns.Behavioral.ChainOfResponsibility.TutorialsPoint;
+
+public class FileLogger extends AbstractLogger {
+
+	   public FileLogger(int level){
+	      this.level = level;
+	   }
+
+	   @Override
+	   protected void write(String message) {		
+	      System.out.println("File::Logger: " + message);
+	   }
+
+}
