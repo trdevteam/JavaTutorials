5353d8b8a5f6916782926e248c1673bcd1af5207
tarikmurat <tarikmurat.islam@gmail.com>
2017-03-26 14:27:52 +0300

    date time

--- JavaTutorial/src/Java8/TimeApi/DateAPIUtilities.java
+++ JavaTutorial/src/Java8/TimeApi/DateAPIUtilities.java
@@ -4,10 +4,29 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class DateAPIUtilities {
 
 	public static void main(String[] args) {
+		
+		TimeZone timeZone1 = TimeZone.getTimeZone("Europe/Istanbul");
+		TimeZone timeZone2 = TimeZone.getTimeZone("Europe/London");
+
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTimeZone(timeZone1);
+		long timeCPH = calendar.getTimeInMillis();
+		System.out.println("timeCPH  = " + timeCPH);
+		System.out.println("hour     = " + calendar.get(Calendar.HOUR_OF_DAY));
+
+		calendar.setTimeZone(timeZone2);
+
+		long timeLA = calendar.getTimeInMillis();
+		System.out.println("timeLA   = " + timeLA);
+		System.out.println("hour     = " + calendar.get(Calendar.HOUR_OF_DAY));
+	
 		
 		LocalDate today = LocalDate.now();
 		
