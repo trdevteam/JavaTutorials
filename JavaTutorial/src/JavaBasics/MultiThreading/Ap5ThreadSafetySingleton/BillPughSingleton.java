8363ddfe2544a06adc0b5b179cc6190bcdc5b765
tarikmurat <tarikmurat.islam@gmail.com>
2017-09-12 23:16:15 +0300

    multithreading

---
+++ JavaTutorial/src/JavaBasics/MultiThreading/Ap5ThreadSafetySingleton/BillPughSingleton.java
@@ -0,0 +1,16 @@
+package JavaBasics.MultiThreading.Ap5ThreadSafetySingleton;
+
+public class BillPughSingleton {
+
+    private BillPughSingleton(){
+    	System.out.println("Bill Paugh");
+    }
+    
+    private static class SingletonHelper{
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+    
+    public static BillPughSingleton getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+}
--- JavaTutorial/src/financial/collateral/database/IDatabase.java
+++
@@ -1,9 +0,0 @@
-package financial.collateral.database;
-
-public interface IDatabase {
-
-	public void update();
-	
-	public String select();
-	
-}
